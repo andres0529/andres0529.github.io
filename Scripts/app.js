8e17d7cdad675e70224832ee098f43af334f0843
Andres Correa <andrescorrea.m@hotmail.com>
2022-10-13 19:25:30 -0400

    experience style and functionality done

--- Scripts/app.js
+++ Scripts/app.js
@@ -19,6 +19,7 @@ var data = {
     aboutPage: {
         title: "About Me",
         button: "Resume",
+        linkResume: "https://www.dropbox.com/s/at3djtjixix6lzk/AndresCorrea_Resume%20August-2022.pdf?dl=0",
         urlImage: "./../../Assets/img/me.jpg",
         paragraphs: [
             ` Hello, My name is Andres and I enjoy creating experiences
@@ -35,7 +36,7 @@ var data = {
     visual Basica and c++, and since then I have been at the
     forefront of technology trying to learn more and more.`,
             `   I had the opportunity to work for some companies such as
-    <a href="http://">Globant</a>, <a
+    <a href="https://www.globant.com/">Globant</a>, <a
         href="https://conexcol.net.co/">Conexcol Cloud</a>,
     <a href="https://www.bdo.ca/en-ca/home/">BDO Canada</a>, among
     others,
@@ -45,6 +46,57 @@ var data = {
     continue growing professionally.`,
         ],
     },
+    experiencePage: [
+        {
+            id: "conexcol",
+            companyName: "Conexcol Cloud",
+            position: "Support Engineer",
+            date: "Feb 2010 - Nov 2016",
+            description: `Conexcol is a company oriented to service infrastructure, in which I participate by performing the  tasks:`,
+            tasks: [
+                "Collaborated with the Design team to create new application features using React Js, Redux and Ant Design.",
+                "Interfaced directly with customers to solve their technical issues through empathetic problem solving.",
+                "Developed company website and customer orientation Deployed mail servers, SSL certificate installation, CAL license configuration, port manipulation and DNS records.",
+                "Broke down and evaluated user problems, using test scripts, personal expertise and probing questions.",
+            ],
+        },
+        {
+            id: "paradigma",
+            companyName: "Paradigma Solutions",
+            position: "Web Developer",
+            date: "Jul 2019 - Apr 2020",
+            description: `Paradigma is a family of programmers dedicated to developing any type of software or web design that is required. Some of the tasks in which I had the opportunity to participate are:`,
+            tasks: [
+                "Maintenance and deployment of DOT NET NUKE components for the Argos pages argos.co, grupoargos.com, honduras.argos.co and argos-us.com",
+                "Developed Single Page Apps (SPAs) using React Js, MaterialUI and ReactHooks.",
+                "I collaborated in the creation of React components for the web application of the Comptroller of Colombia https://www.contraloria.gov.co/es/web/guest/atencion-al-ciudadano/denuncias-y-otras-solicitudes-pqrd using Jest and Huskey as testing frameworks and Redux for application state management",
+                "developed some components in Angular CLI for the creation of the Kitchensink library, a library for internal use of the company for the development of applications",
+            ],
+        },
+        {
+            id: "globant",
+            companyName: "Globant",
+            position: "Web UI Developer",
+            date: "Dec 2020 - Nov 2021",
+            description: `Globant is a publicly traded software development agency focused on serving corporate clients worldwide. My role as a UI developer had several important tasks like:`,
+            tasks: [
+                "I collaborated in the migration of 2 applications in AngularJS to ReactJS, using technologies such as Redux, Hooks, Typescript, Recharts and I was in the complete life cycle of the project.",
+                "Technologies such as StoryBookJS and Jest for unit tests were used for the development of each component, which allowed testing the components created before integration.",
+                "The client for which we developed the migration is American Century Investment https://www.americancentury.com/home/, with whom we had weekly meetings to show the progress of the project using the SCRUM methodology.",
+            ],
+        },
+        {
+            id: "bdo",
+            companyName: "BDO Canada",
+            position: "Software and Web Developer, Intern",
+            date: "May 2022 - Aug 2022",
+            description: `BDO or Binder Dijker Otte is an international network of public accounting, tax, consulting and business advisory firms. Some of the tasks performed were:`,
+            tasks: [
+                "To give back and support communities in need, BDO Canada annually launches the Drive Away Hunger campaign with a goal to donate 1 million meals to food banks across Canada. For fundraising, the DAH application was developed from its design to its deployment.",
+                "Complete creation of the Drive Away Hunger application, which is based on ReactJS components, React Router, Hooks, API Context, Bootstrap, Material UI as FrontEnd and ASP.NET 6 and SQL for the BackEnd",
+            ],
+        },
+    ],
 };
 /*=============================================
     OBJECT WITH METHODS
@@ -126,6 +178,48 @@ let m = {
                     $("main #about .paragraphs").append(buttonResume);
                     break;
                 case "experience":
+                    let jobbtnsContainer = document.querySelector(".job-btns");
+                    let jobDescription = document.querySelector(".jobDescription p");
+                    let companyName = document.querySelector(".headings h5 ");
+                    let jobDate = document.querySelector(".headings span");
+                    let jobTasks = document.querySelector(".jobDescription ul");
+                    let joinedTasks = "";
+                    //****** Method to create P and take texts from DATAA (yhis method is called for every Click) ****/
+                    let addInfo = (button) => {
+                        //Match the job Id with the Button Id
+                        let jobSelected = data.experiencePage.find((job) => job.id === button.target.id);
+                        //Nesxt 3 lines to clean the button selected and the paragraph
+                        $(".activeButton").removeClass("activeButton");
+                        $(`[id= '${button.target.id}']`).addClass("activeButton");
+                        joinedTasks = "";
+                        //Populate the buttons with the job selected
+                        jobDescription.textContent = jobSelected.description;
+                        companyName.textContent = jobSelected.companyName;
+                        jobDate.textContent = jobSelected.date;
+                        //build the string with all the tasks together separated by P element each one
+                        jobSelected.tasks.map((task) => {
+                            joinedTasks += `<li class="mt-4">${task}</li>`;
+                        });
+                        //add the string created befor as HTML inside the UL
+                        jobTasks.innerHTML = joinedTasks;
+                    };
+                    //****** Method to create Buttons accoridng to the data ****/
+                    data.experiencePage.map((job) => {
+                        let buttonElement = `<button id="${job.id}" type="button" class="btn btn-outline-secondary">${job.companyName}</button>`;
+                        jobbtnsContainer.innerHTML += buttonElement; //adding next button + before
+                    });
+                    //add a listener to each buttons in orden to call the function AddInfo
+                    let buttons = document.querySelectorAll("#experience button");
+                    [...buttons].map((button) => {
+                        button.addEventListener("click", addInfo);
+                    });
+                    //***Load the informatrion by default first time in the page***
+                    document.querySelector(".job-btns button").classList.add("activeButton");
+                    companyName.textContent = data.experiencePage[0].companyName;
+                    jobDescription.textContent = data.experiencePage[0].description;
+                    jobDate.textContent = data.experiencePage[0].date;
+                    data.experiencePage[0].tasks.map((task) => (joinedTasks += `<li class="mt-4">${task}</li>`));
+                    jobTasks.innerHTML = joinedTasks;
                     break;
                 default:
                     break;
